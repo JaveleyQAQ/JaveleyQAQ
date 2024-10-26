68d4fcb92f59d539923af70212e81a27b14cbaad
Javeley <132129852+JaveleyQAQ@users.noreply.github.com>
2024-10-26 16:15:47 +0800

    Update demo.js

--- images/demo.js
+++ images/demo.js
@@ -1,2 +1,14 @@
+/*
+QuantumultX:
+[rewrite_local]
+^https?:\/\/accounts\.melon\.com\/myinfo\/setting_ageauth url script-response-body https://raw.githubusercontent.com/JaveleyQAQ/JaveleyQAQ/refs/heads/main/images/demo.js
+
+[mitm]
+hostname = *account.wps.cn, *accounts.melon.com
+
+**************************/
+
+var body = JSON.parse($response.body);
+
 body = $response.body.replace('c', '1231231313')
 $done({body});
