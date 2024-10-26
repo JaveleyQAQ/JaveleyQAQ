48e652524a05e24ab2dd3f260b82256d6a540ba7
Javeley <132129852+JaveleyQAQ@users.noreply.github.com>
2024-10-26 15:50:32 +0800

    Update demo.js

--- images/demo.js
+++ images/demo.js
@@ -1,13 +1,2 @@
-/******************************
-[rewrite_local]
-
-^https?:\/\/accounts\.melon\.com\/myinfo\/setting_ageauth url script-response-body https://github.com/JaveleyQAQ/JaveleyQAQ/edit/main/images/demo.js
-
-[mitm] 
-
-hostname = accounts.melon.com
-
-*******************************/
-
 body = $response.body.replace(/<dd>(c.*?)<\/dd>/g, '1231231313')
 $done({body});
