1ff5e0812b78f0fe25084af8ef05657b1f66fcae
Javeley <132129852+JaveleyQAQ@users.noreply.github.com>
2024-10-26 15:46:44 +0800

    Update demo.js

--- images/demo.js
+++ images/demo.js
@@ -1,1 +1,14 @@
-$notification.post("Emby Premiere 已激活", "", "");
+/******************************
+*******************************
+[rewrite_local]
+
+^https?:\/\/accounts\.melon\.com\/myinfo\/setting_ageauth url script-response-body https://github.com/JaveleyQAQ/JaveleyQAQ/edit/main/images/demo.js
+
+[mitm] 
+
+hostname = accounts.melon.com
+
+*******************************/
+
+body = $response.body.replace(/<dd>(c.*?)</dd>/g, '1231231313')
+$done({body});
